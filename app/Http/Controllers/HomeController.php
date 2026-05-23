a5c9c933cf44d37ed6daccd302e4a9a663e6d476
Rashidul Islam <rashidul.191cse.gub@gmail.com>
2026-05-23 15:11:06 +0600

    update more file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,7 +20,7 @@ class HomeController extends Controller
         $data['notices'] = Notice::latest()->get();
         $data['activities'] = Activity::oldest('serial')->get();
         $data['services'] = Service::oldest('serial')->get();
-        $data['galleryImages'] = Gallery::latest()->take(8)->get();
+        $data['galleryImages'] = Gallery::latest()->take(12)->get();
         $data['videoGalleries'] = VideoGallery::oldest('serial')->get();
         $data['testimonials'] = Testimonial::latest()->take(12)->get();
         $data['blogs'] = Blog::latest()->take(5)->get();
--- resources/views/front-end/home-page-section/slider.blade.php
+++ resources/views/front-end/home-page-section/slider.blade.php
@@ -8,13 +8,14 @@
                         <!-- Content Column -->
                         <div class="content-column col-lg-7 col-md-12 col-sm-12">
                             <div class="inner-column">
-                                <div class="title">{!! $item->sub_title ?? '' !!}</div>
-                                <h1>{!! $item->title ?? '' !!}</h1>
-                                <div class="text">{!! $item->short_description ?? '' !!}</div>
-                                @if($item->btn_text)
-                                    <div class="btn-box">
-                                        <a href="{{ $item->link ?? 'javascript:void(0)' }}" class="theme-btn btn-style-one">
-                                            <span class="txt">{!! $item->btn_text ?? '' !!}</span>
+                                {{-- <div class="title">{!! $item->sub_title ?? '' !!}</div> --}}
+                                <h1 class="text-white fw-semibold">{!! $item->title ?? '' !!}</h1>
+                                {{-- <div class="text">{!! $item->short_description ?? '' !!}</div> --}}
+                                @if($item->page_link)
+                                    <div class="btn-box mt-3">
+                                        <a href="{{ $item->page_link ?? 'javascript:void(0)' }}"
+                                            class="theme-btn btn-style-one">
+                                            <span class="txt">{!! $item->btn_text ?? 'Learn More' !!}</span>
                                         </a>
                                     </div>
                                 @endif
